8de97d4fdd0469418e691f217e5cb383d0151b61
Pirooz Wallace <PiroozWallace@outlook.com>
2020-10-14 16:45:07 -0700

    variables and functions
    added arrays for password generation as well as pulling values from the control form. started skeleton for generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+
+/* Initialize booleans for generation--whether or not to include lowercase,
+uppercase, numbers and/or special characters */
+
+isLowercase = document.getElementById("lowercase")
+isUppercase = document.getElementById("uppercase")
+isNumbers = document.getElementById("numbers")
+isSpecial = document.getElementById("specialChar")
+
+
+/* Create Password function given the conditionals */
+function generatePassword()
+{
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
